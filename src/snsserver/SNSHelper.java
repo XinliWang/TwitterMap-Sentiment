eab6c15bf0cecae571d8c6806d8779f552651d63
Xinli Wang <wxlmaple@hotmail.com>
2015-11-30 00:06:04 -0500

    SNS and servlet

---
+++ src/snsserver/SNSHelper.java
@@ -0,0 +1,25 @@
+package snsserver;
+
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sns.model.ConfirmSubscriptionRequest;
+import com.amazonaws.services.sns.model.ConfirmSubscriptionResult;
+
+public enum SNSHelper {
+	INSTANCE;
+	
+	AWSCredentials credentials = new ProfileCredentialsProvider("default").getCredentials();
+	private AmazonSNSClient amazonSNSClient = new AmazonSNSClient(credentials);
+	
+	public void confirmTopicSubmission(SNSmessage message) {
+		ConfirmSubscriptionRequest confirmSubscriptionRequest = new ConfirmSubscriptionRequest()
+		 							.withTopicArn(message.getTopicArn())
+									.withToken(message.getToken());
+		ConfirmSubscriptionResult resutlt = amazonSNSClient.confirmSubscription(confirmSubscriptionRequest);
+		System.out.println("subscribed to " + resutlt.getSubscriptionArn());
+		
+	}
+	
+}
