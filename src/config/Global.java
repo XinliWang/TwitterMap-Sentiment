cade61f75f357b2018087b427f9478969f8840fc
Xinli Wang <wxlmaple@hotmail.com>
2015-11-30 00:09:16 -0500

    Global configuration

---
+++ src/config/Global.java
@@ -0,0 +1,17 @@
+package config;
+
+public class Global {
+	
+	public static final String dbName = "";
+	public static final String userName = "";
+	public static final String password = "";
+	public static final String hostname = "";
+	public static final String port = "";
+
+	public static final String TwitterConsumerKey = "";
+	public static final String TwitterConsumerSecret="";
+	public static final String TwitterAccessToken="";
+	public static final String TwitterAccessTokenSecret="";
+
+	public static final String AlchemyKEY = "";
+}
